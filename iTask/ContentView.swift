1f07e9632022b856f9e6b629f5ef0c618c4012f1
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-05-27 01:03:54 +0200

    Initial Commit

---
+++ iTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTask
+//
+//  Created by Julian Hermanspahn on 27.05.20.
+//  Copyright © 2020 Julian Hermanspahn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
