7624662164a3019280c170d11b4e8d0979d2a49c
Thomas Raab <thomas.raab@mni.thm.de>
2020-05-28 21:03:36 +0200

    Tabview

---
+++ iTask/FreundeView.swift
@@ -0,0 +1,21 @@
+//
+//  FreundeView.swift
+//  iTask
+//
+//  Created by Thomas on 28.05.20.
+//  Copyright © 2020 Julian Hermanspahn. All rights reserved.
+//
+
+import SwiftUI
+
+struct FreundeView: View {
+    var body: some View {
+        Text("Freunde")
+    }
+}
+
+struct FreundeView_Previews: PreviewProvider {
+    static var previews: some View {
+        FreundeView()
+    }
+}
---
+++ iTask/HeuteView.swift
@@ -0,0 +1,21 @@
+//
+//  HeuteView.swift
+//  iTask
+//
+//  Created by Thomas on 28.05.20.
+//  Copyright © 2020 Julian Hermanspahn. All rights reserved.
+//
+
+import SwiftUI
+
+struct HeuteView: View {
+    var body: some View {
+        Text("Heute")
+    }
+}
+
+struct HeuteView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeuteView()
+    }
+}
