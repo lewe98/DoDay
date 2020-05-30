1a01a7b3c81d79cdc50236dce334fb9a4aa7338a
T Raab <raabt@gmx.net>
2020-05-30 18:12:03 +0200

    Der View des Freunde- und Einstellungen-Tabs ist optisch fertig. Funktionen fehlen noch.

--- iTask/EinstellungenView.swift
+++ iTask/EinstellungenView.swift
@@ -9,8 +9,105 @@
 import SwiftUI
 
 struct EinstellungenView: View {
+    @State private var erinnerungEigeneAktiviert = false
+    @State private var erinnerungFreundeAbgeschlossenAktiviert = false
+    @State private var erinnerungFreundeProblemeAktiviert = false
+    
     var body: some View {
-        Text("Einstellungen")
+        VStack(alignment: .leading,spacing: 12) {
+            
+            Text("Einstellungen").fontWeight(.heavy).font(.largeTitle)
+            .padding(.horizontal)
+            .padding(.top,92)
+            
+            Form {
+                Section {
+                    HStack {
+                        // TODO: Hier Erinnerung wirklich togglen
+                        Toggle(isOn: $erinnerungEigeneAktiviert) {
+                            VStack(alignment: .leading) {
+                                Text("Meine Erinnerung")
+                                    .font(.headline)
+                                Text("Täglich um 8:00 Uhr")
+                                    .font(.callout)
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
+                    }
+                }
+            }
+            .frame(height: 100.0)
+            .padding(.bottom, 15)
+            
+            
+            Form {
+                Section(header: Text("INFORMIERE MICH, WENN FREUNDE...")) {
+                    List {
+                      Toggle(isOn: $erinnerungFreundeAbgeschlossenAktiviert) {
+                            Text("Aufgaben abgeschlossen haben")
+                            .font(.headline)
+                        }
+                        Toggle(isOn: $erinnerungFreundeProblemeAktiviert) {
+                            Text("Probleme mit Aufgaben haben")
+                            .font(.headline)
+                        }
+                    }
+                }
+            }
+            .frame(height: 150.0)
+
+            Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // TODO: Funktion einfuegen
+                            print("Aufgabe einreichen tapped")
+                        }) {
+                            Text("Aufgabe einreichen")
+                        }
+                        Spacer()
+                    }
+                }
+                Section {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // TODO: Funktion einfuegen
+                            print("Statistik zurücksetzen tapped")
+                        }) {
+                            Text("Statistik zurücksetzen")
+                                .foregroundColor(.red)
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            .frame(height: 200.0)
+            
+            Spacer()
+            
+            HStack{
+                Button(action: {
+                    // TODO: Funktion einfuegen
+                    print("Imoressum tapped")
+                }) {
+                    Text("Impressum")
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Button(action: {
+                    // TODO: Funktion einfuegen
+                    print("Datenschutz tapped")
+                }) {
+                    Text("Datenschutz")
+                        .foregroundColor(.gray)
+                }
+            }.padding(.horizontal)
+            Spacer()
+        }
+        .background(Color(UIColor.secondarySystemBackground))
+            .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
 
--- iTask/FreundeView.swift
+++ iTask/FreundeView.swift
@@ -9,9 +9,120 @@
 import SwiftUI
 
 struct FreundeView: View {
+    //MARK: Muss irgendwo anders definiert werden
+    let currUser = UserModel(id: "LgUZK2f", name: "Thomas Raab", aufgabe: 2, aktueller_streak: 3, letztes_erledigt_datum: "2020-05-29", anzahl_benachrichtigungen: 0)
+    
+    let currAufgabe = AufgabeModel(id: 1, text: "Sprich mit einer fremden Person", text_dp: "Spricht mit einer fremden Person", detailtext: "Fördere deine Sozialkompetenz, indem du mit einer Person sprichst, mit der du vorher noch nie gesprochen hast. Zum Beispiel dein Postbote, jemand beim Einkaufen usw.", kategorie: "Social", autor: "admin", ausgespielt: 1, erledigt: 0, abgelehnt: 0, aufgeschoben: 0)
+    
     var body: some View {
-        Text("Freunde")
+        VStack(alignment: .leading,spacing: 12) {
+            
+            HStack {
+                Text("Freunde").fontWeight(.heavy).font(.largeTitle)
+                Spacer()
+                Image(systemName: "plus.circle")
+                .font(.title)
+                .foregroundColor(.blue)
+            }.padding(.horizontal)
+            .padding(.top,92)
+            
+            Form {
+                Section(header: Text("DEIN FREUNDESCODE")) {
+                    HStack {
+                        Text(currUser.id)
+                        Spacer()
+                        Button(action: {
+                            UIPasteboard.general.string = self.currUser.id
+                        }) {
+                            Text("Kopieren")
+                        }
+                    }
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            print("Freunde hinzufügen tapped")
+                        }) {
+                            Text("Freunde hinzufügen")
+                        }
+                        Spacer()
+                    }
+                }
+            }.padding(.top,20)
+                .padding(.bottom, 15)
+            
+            Form {
+                Section(header: HStack {
+                    Text("FREUNDE")
+                    Spacer()
+                    Text("ERLEDIGTE AUFGABEN")
+                    }) {
+                     // TODO: Liste dynamisch füllen mit for schleife
+                    List {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(currUser.name)
+                                    .font(.headline)
+                                Text(currAufgabe.text_dp)
+                                    .font(.callout)
+                                    .foregroundColor(Color.gray)
+                            }
+                            Spacer()
+                            // TODO: Dynamisch machen
+                            Text("6")
+                        }
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(currUser.name)
+                                    .font(.headline)
+                                Text(currAufgabe.text_dp)
+                                    .font(.callout)
+                                    .foregroundColor(Color.gray)
+                            }
+                            Spacer()
+                            Text("22")
+                        }
+                    }
+                }
+            }
+            //.padding([.top],30)
+            //.padding(.bottom, 15)
+            Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("Fordere deine Freunde raus tapped")
+                        }) {
+                            Text("Fordere deine Freunde heraus")
+                        }
+                        Spacer()
+                    }
+                }
+            }.padding([.top],30)
+            .padding(.bottom, 15)
+            Spacer()
+            .frame(height: 50)
+            Spacer()
+        }.background(Color(UIColor.secondarySystemBackground))
+            .edgesIgnoringSafeArea([.top, .bottom])
+        
     }
+}
+
+func freundeHinzufuegenTapped() {
+    /*let ac = UIAlertController(title: "Hier Freundescode einfügen", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+
+    let submitAction = UIAlertAction(title: "Hinzufügen", style: .default) { [unowned ac] _ in
+        let answer = ac.textFields![0]
+        // do something interesting with "answer" here
+ 
+    }
+
+    ac.addAction(submitAction)
+
+    self.present(ac, animated: true, completion: nil)
+ */
 }
 
 struct FreundeView_Previews: PreviewProvider {
