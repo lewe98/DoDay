75dde651337d5fe29253fb8e698e60aa67b9bac3
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-07-08 00:46:12 +0200

    reset & friend list fix

--- iTask/FreundeViews/FreundeHinzufuegenView.swift
+++ iTask/FreundeViews/FreundeHinzufuegenView.swift
@@ -41,20 +41,20 @@ struct FreundeHinzufuegenView: View {
                         HStack {
                             Spacer()
                             Button(action: {
-                                if self.freundesCode == self.globalFunctions.coreDataFunctions.curUser.freundes_id {
+                                if self.freundesCode.trimmingCharacters(in: .whitespacesAndNewlines) == self.globalFunctions.coreDataFunctions.curUser.freundes_id {
                                     self.alertTitle = "Ungültige ID."
                                     self.alertText = "Die eigene ID kann nicht hinzugefügt werden."
                                     self.showingAlert = true
-                                } else if self.globalFunctions.coreDataFunctions.curUser.freunde.contains(self.freundesCode) {
+                                } else if self.globalFunctions.coreDataFunctions.curUser.freunde.contains(self.freundesCode.trimmingCharacters(in: .whitespacesAndNewlines)) {
                                     self.alertTitle = "Ungültige ID."
                                     self.alertText = "ID ist bereits in deiner Freundesliste."
                                     self.showingAlert = true
-                                } else if self.freundesCode == "" {
+                                } else if self.freundesCode.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
                                     self.alertTitle = "Leeres Feld."
                                     self.alertText = "Bitte gib einen Freundescode ein."
                                     self.showingAlert = true
-                                } else if !self.globalFunctions.coreDataFunctions.curUser.freunde.contains(self.freundesCode) {
-                                    self.globalFunctions.callAddFriend(freundID: self.freundesCode)
+                                } else if !self.globalFunctions.coreDataFunctions.curUser.freunde.contains(self.freundesCode.trimmingCharacters(in: .whitespacesAndNewlines)) {
+                                    self.globalFunctions.callAddFriend(freundID: self.freundesCode.trimmingCharacters(in: .whitespacesAndNewlines))
                                     
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                         self.presentation.dismiss()
